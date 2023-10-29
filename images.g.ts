7bc55f2193d3070849ee4c4ec7481d1a28136056
graciep1937 <147439186+graciep1937@users.noreply.github.com>
2023-10-29 07:57:54 +0000

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "bob the builder":return img`
+. . . e e e e e e e e e . . . . 
+. . 1 1 6 1 1 1 1 1 6 1 1 . . . 
+. . 1 1 1 1 1 f 1 1 1 1 1 . . . 
+. . . 2 1 1 1 1 1 1 1 2 . . . . 
+. . . 1 2 2 2 2 2 2 2 1 . . . . 
+. 7 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. 1 7 7 7 7 7 7 7 7 7 7 7 1 . . 
+. 1 . 7 7 7 7 7 7 7 7 7 . 1 . . 
+. 1 . f f f f 5 f f f f . 1 . . 
+. 1 . 8 8 8 8 8 8 8 8 8 . 1 . . 
+. . . 8 8 8 . . . 8 8 8 . . . . 
+. . . 8 8 8 . . . 8 8 8 . . . . 
+. . . 8 8 8 . . . 8 8 8 . . . . 
+. . . f f f . . . f f f . . . . 
+. . f f f f . . . f f f f . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
